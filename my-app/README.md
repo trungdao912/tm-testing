dad65690109148cd797fc3c06a9daaa0fe6221ae
Theo Dao <trungdao912@gmail.com>
2021-03-09 22:42:51 +0700

    implementation test2

--- my-app/README.md
+++ my-app/README.md
@@ -1,6 +1,6 @@
-## How to change dynamic data
+## TEST 1
 Changing ~/mock/data.json to change the structure of the dropdown menu
-
+~/src/components/Menu is the menu component for test 1
 The structure of the data must comply this structure
 ```json
 [
@@ -24,3 +24,6 @@ The structure of the data must comply this structure
   ...
 ]
 ```
+
+## TEST 2
+~/src/App.js
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,44 +1,87 @@
-import React, { useState, useEffect } from "react";
-import Web3 from "web3";
-import Menu from "./components/Menu";
-import data from "./mock/data.json";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import Menu from './components/Menu';
+import data from './mock/data.json';
+import './App.css';
 
 function App() {
-  const [walletAddress, setWalletAddress] = useState("");
-  const [error, setError] = useState("");
-  const web3 = new Web3("https://rpc.tomochain.com");
+  const [walletAddress, setWalletAddress] = useState('');
+  const [subscribeVal, setSubscribeVal] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [walletBalance, setWalletBalance] = useState(0);
+  const [error, setError] = useState('');
+  const web3 = new Web3('https://rpc.tomochain.com');
 
   useEffect(() => {
-    if (walletAddress) {
-      try {
-        web3.eth.getBalance(walletAddress).then((res) => {
-          console.log(res);
-        });
-      } catch (error) {
-        setError(error && error.message);
-      }
+    let timeout = null;
+    if (subscribeVal) {
+      timeout = setInterval(() => {
+        try {
+          web3.eth.getBalance(subscribeVal).then((data) => {
+            setLoading(false);
+            setError('');
+            setWalletBalance(data);
+          });
+        } catch (error) {
+          setLoading(false);
+          setError(error.message);
+          setWalletBalance(0);
+          clearInterval(timeout);
+        }
+      }, 2000);
     }
-  }, [walletAddress]);
+
+    // Clean up function
+    return () => {
+      if (timeout) {
+        clearInterval(timeout);
+      }
+    };
+  }, [subscribeVal]);
 
   return (
     <div className="App">
+      {/* Test 1 */}
       <Menu data={data} />
-      <input
-        value={walletAddress}
-        onChange={(e) => {
-          setWalletAddress(e.target.value);
+      {/* Test 2 */}
+      <div
+        style={{
+          padding: '20px',
         }}
-      />
-      {error && (
+      >
         <div
           style={{
-            color: "red",
+            display: 'flex',
+            marginBottom: '5px',
           }}
         >
-          {error}
+          <input
+            value={walletAddress}
+            onChange={(e) => {
+              setWalletAddress(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              setSubscribeVal(walletAddress);
+              setLoading(true);
+            }}
+          >
+            {loading ? 'Loading...' : 'Search'}
+          </button>
         </div>
-      )}
+
+        {error && (
+          <div
+            style={{
+              color: 'red',
+            }}
+          >
+            {error}
+          </div>
+        )}
+        <div>Wallet's Balance: {walletBalance !== 0 ? web3.utils.fromWei(walletBalance) : 0} TOMO</div>
+      </div>
     </div>
   );
 }
