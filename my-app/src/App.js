76eb646ffdb9e6db5d5886a1f7af1fbc463f0fbf
Trung Dao <advesa@Advesas-Mac-mini.local>
2021-03-09 14:20:04 +0700

    finishing feature + adding readme

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,65 +1,13 @@
-import Menu from './components/Menu';
-import './App.css';
-
-const mockData = [
-  {
-    title: 'Menu 1',
-    children: [
-      {
-        title: 'Menu 1 - Sub1',
-        children: [
-          {
-            title: 'Menu 1 - sub1 -sub1',
-          },
-          {
-            title: 'Menu 1 - sub1 -sub2',
-          },
-        ],
-      },
-      {
-        title: 'Menu 1 - Sub2',
-      },
-      {
-        title: 'Menu 1 - Sub3',
-      },
-    ],
-  },
-  {
-    title: 'Menu 2',
-    children: [
-      {
-        title: 'Menu 1 - Sub1',
-      },
-      {
-        title: 'Menu 1 - Sub2',
-      },
-      {
-        title: 'Menu 1 - Sub3',
-      },
-    ],
-  },
-  {
-    title: 'Menu 3',
-    children: [
-      {
-        title: 'Menu 1 - Sub1',
-      },
-      {
-        title: 'Menu 1 - Sub2',
-      },
-      {
-        title: 'Menu 1 - Sub3',
-      },
-    ],
-  },
-];
+import Menu from './components/Menu'
+import './App.css'
+import data from './mock/data.json'
 
 function App() {
   return (
     <div className="App">
-      <Menu data={mockData} />
+      <Menu data={data} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- my-app/src/components/Menu/index.js
+++ my-app/src/components/Menu/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import styles from './styles.module.scss';
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+
+const ChildMenuItem = ({ item = {}, depth = 0, index }) => {
+  const { children } = item;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+      style={{ position: "relative" }}
+    >
+      <li className={styles.navSubmenuItem}>
+        {item.title}
+        {children && children.length !== 0 && show && (
+          <ul depth={depth} index={index} className={styles.navChildrenMenu}>
+            {children.map((c) => {
+              return <ChildMenuItem item={c} depth={depth + 1} />;
+            })}
+          </ul>
+        )}
+      </li>
+    </div>
+  );
+};
 
 const MenuItem = ({ data }) => {
   const renderSubMenuItem = (items) => {
     return (
       <>
         <ul className={styles.navSubmenu}>
-          {items.map((item) => {
-            const { children } = item
+          {items.map((item, index) => {
             return (
-              <li className={styles.navSubmenuItem}>
-                {item.title}
-              </li>
+              <div>
+                <ChildMenuItem item={item} index={index} />
+              </div>
             );
           })}
         </ul>
@@ -28,7 +51,9 @@ const MenuItem = ({ data }) => {
             <>
               <li className={styles.navMenuItem}>
                 {item.title}
-                {children && children.length !== 0 && renderSubMenuItem(children)}
+                {children &&
+                  children.length !== 0 &&
+                  renderSubMenuItem(children)}
               </li>
             </>
           );
